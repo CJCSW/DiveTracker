bb4b8dfb4edd29416168a568855cc4c9b7e6e544
Juan Carlos Molina <jcarlosmolina@gmail.com>
2012-02-26 23:51:06 +0100

    CertificationListActivity y EquipmentListActivity empotradas en pestañero en UserDetailsActivity

--- src/org/cjc/mydives/divetracker/UserDetailsActivity.java
+++ src/org/cjc/mydives/divetracker/UserDetailsActivity.java
@@ -8,16 +8,17 @@ import static org.cjc.mydives.divetracker.db.UserConstants.FIELD_SURNAME;
 import org.cjc.mydives.divetracker.db.EquipmentConstants;
 import org.cjc.mydives.divetracker.db.UserDbAdapter;
 
-import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 
@@ -28,7 +29,7 @@ import android.widget.TextView;
  * @author JuanCarlos
  *
  */
-public class UserDetailsActivity extends Activity {
+public class UserDetailsActivity extends TabActivity {
 	private UserDbAdapter userDbAdapter;
 	
 	private Long rowId;
@@ -39,8 +40,12 @@ public class UserDetailsActivity extends Activity {
 	
 	private ImageView ivEdit;
 	private ImageView ivAdd;
+	//JC-INI
+	/*
 	private Button btEquipment;
 	private Button btCertifications;
+	*/
+	//JC-FIN
 	
 	private static int ACTION_EDIT = 10;
 	
@@ -57,8 +62,12 @@ public class UserDetailsActivity extends Activity {
         
         ivEdit = (ImageView) this.findViewById(R.id.header_button_edit);
 		ivAdd  = (ImageView) this.findViewById(R.id.header_button_add);
+		//JC-INI
+		/*
 		btEquipment = (Button)findViewById(R.id.user_details_button_equipment);
 		btCertifications = (Button)findViewById(R.id.user_details_button_certifications);
+		*/
+		//JC-FIN
         
         userDbAdapter = new UserDbAdapter(this);
 
@@ -79,6 +88,39 @@ public class UserDetailsActivity extends Activity {
 		
 		// Populate fields
 		populateFields();
+		
+		// Set up tabbed view
+		setUpTabs();
+	}
+	
+	private void setUpTabs() {
+		// Tabbed view
+        Resources res = getResources();	// Resources object to get access to strings and graphic resources
+        TabHost tabHost = getTabHost();	// The TabHost defined in the layout used as content view for this activity
+        TabHost.TabSpec tabSpec;		// Reusable TabSpec to define each of the tabs in this activity's TabHost
+        Intent tabIntent;				// Reusable Intent to launch the activity for each of the tabs
+        
+        // Certifications tab
+        // Create an intent to launch the CertificationListActivity
+        tabIntent = new Intent().setClass(this, CertificationListActivity.class);
+        // Create a TabSpec for the Dives tab and add it to the TabHos
+        tabSpec = tabHost.newTabSpec(res.getString(R.string.user_details_tab_tag_certifications))
+        		.setIndicator(res.getString(R.string.user_details_tab_certifications), res.getDrawable(R.drawable.ic_tab_certifications))
+        		.setContent(tabIntent);
+        tabHost.addTab(tabSpec);        
+
+        // Equipment tab
+        // Create an intent to launch the EquipmentListActivity
+        tabIntent = new Intent().setClass(this, EquipmentListActivity.class);
+        tabIntent.putExtra(EquipmentConstants.FIELD_USERID, rowId);
+        // Create a TabSpec for the User tab and add it to the TabHos
+        tabSpec = tabHost.newTabSpec(res.getString(R.string.user_details_tab_tag_equipment))
+        		.setIndicator(res.getString(R.string.user_details_tab_equipment), res.getDrawable(R.drawable.ic_tab_equipment))
+        		.setContent(tabIntent);
+        tabHost.addTab(tabSpec);     
+        
+        // Set Home as the current tab
+        tabHost.setCurrentTab(0);
 	}
 	
 	private void addListeners(){
@@ -109,6 +151,8 @@ public class UserDetailsActivity extends Activity {
 			}
 		});
 		
+		//JC-INI
+		/*
 		// Equipment button
 		btEquipment.setOnClickListener(new View.OnClickListener() {
 			
@@ -128,6 +172,8 @@ public class UserDetailsActivity extends Activity {
 				startActivity(new Intent(v.getContext(), CertificationListActivity.class));
 			}
 		});
+		*/
+		//JC-FIN
 	}
 	
 	@Override
@@ -167,13 +213,21 @@ public class UserDetailsActivity extends Activity {
     		}
      		ivAdd.setVisibility(View.GONE);
      		ivEdit.setVisibility(View.VISIBLE);
+     		//JC-INI
+     		/*
      		btEquipment.setEnabled(true);
      		btCertifications.setEnabled(true);
+     		*/
+     		//JC-FIN
     	} else {
      		ivAdd.setVisibility(View.VISIBLE);
      		ivEdit.setVisibility(View.GONE);
+     		//JC-INI
+     		/*
      		btEquipment.setEnabled(false);
      		btCertifications.setEnabled(false);
+     		*/
+     		//JC-FIN
     	}
     	userCursor.close();
     	userDbAdapter.close();
